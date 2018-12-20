d69c9e90d22e20f044f4916e3a760b2eba410a79
Sean Usher <seusher@exchange.microsoft.com>
2018-12-20 13:19:00 -0700

    Add FlexVolume code alongside Volume mounted azure.json code

--- Testsecrets/__init__.py
+++ Testsecrets/__init__.py
@@ -16,16 +16,29 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     try:
-        response = getSecret("https://cseboulder.vault.azure.net/", "seushertest")
+        #response = getSecret("https://cseboulder.vault.azure.net/", "seushertest")
+        response = getSecretFlexVolume("seushertest")
         return func.HttpResponse(response)
     except:
         response = f"Unexpected error: {sys.exc_info()[0]}."
         return func.HttpResponse(response, 500)
 
 
+def getSecretFlexVolume(secretName: str) -> str:
+    """
+    Use this when accessing secrets via FlexVolumes (kvvolume)
+
+    Run: kubectl create -f \
+         https://raw.githubusercontent.com/Azure/kubernetes-keyvault-flexvol/master/deployment/kv-flexvol-installer.yaml
+    Grant your SPN 'Get' access on secrets (spn is from /etc/kubernetes/azure.json)
+    Deploy the container
+    """
+    with open(f"/kv/{secretName}", 'r') as secret_text:
+        return secret_text.read()
+
 def getSecret(vaultUri: str, name: str) -> str:
     """
-    Gets a secret from Key Vault
+    Gets a secret from Key Vault when using a Volume to mount /etc/kubernetes/azure.json
     """
 
     def auth_callback(server, resource, scope):
